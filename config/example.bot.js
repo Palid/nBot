25babc582838fd86d3523fffc0361b2f9746c3ee
Dariusz Niemczyk <palid@o2.pl>
2014-02-24 11:11:25 +0100

    added privateMethods

--- config/example.bot.js
+++ config/example.bot.js
@@ -1,7 +1,6 @@
 /*jslint node: true */
 "use strict";
-const irc = require('irc'),
-fs = require('fs');
+var irc = require('irc');
 
 var client = new irc.Client('chat.freenode.net', 'exampleNick', {
     userName: "aBot",
@@ -16,33 +15,6 @@ var client = new irc.Client('chat.freenode.net', 'exampleNick', {
 });
 
 client.commandCharacter = ',';
-client.login = "login";
-client.password = "password";
-client.log = function (name, data) {
-
-    function timestamp() {
-
-        var currentTime = new Date(),
-            hours = currentTime.getHours(),
-            minutes = currentTime.getMinutes(),
-            seconds = currentTime.getSeconds();
-
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
-
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        return "[" + hours + ":" + minutes + ":" + seconds + "]";
-    }
-
-    var time = timestamp();
-
-    fs.appendFile("logs/" + name + ".log", time + " " + data, function (err) {
-        if (err) console.log(err);
-    });
-};
 
 
 module.exports = client;
---
+++ privateMethods/log.js
@@ -0,0 +1,50 @@
+/*jslint node: true */
+'use strict';
+var _ = require('lodash'),
+    fs = require('fs'),
+    path = require('path');
+
+
+var method = function (timestampBool, name, data) {
+
+    function timestamp() {
+
+        var currentTime = new Date(),
+            year = currentTime.getUTCFullYear(),
+            month = currentTime.getUTCMonth() + 1, //month counting starts at 0
+            day = currentTime.getUTCDate(),
+            hours = currentTime.getHours(),
+            minutes = currentTime.getMinutes(),
+            seconds = currentTime.getSeconds();
+
+        if (month < 10) {
+            month = "0" + month;
+        }
+        if (day < 10) {
+            day = "0" + day;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+
+
+        var date = "[" + day + '.' + month + '.' + year + "]";
+        var time = "[" + hours + ":" + minutes + ":" + seconds + "]";
+
+        return date + time;
+    }
+
+    var time = timestamp(),
+        dir = path.resolve(__dirname, "../logs/" + name + ".log"),
+        logString = (timestampBool) ? time + " " + data : data;
+
+    fs.appendFile(dir, logString, function (err) {
+        if (err) console.log(err);
+    });
+};
+
+module.exports = method;

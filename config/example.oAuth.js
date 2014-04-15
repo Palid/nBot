c185047f3dea005e124ad8f91650f7cf144df53d
Dariusz Niemczyk <palid@o2.pl>
2014-04-15 22:06:43 +0200

    add example oAuth

---
+++ config/example.oAuth.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var oAuth = {
+    'tumblr': {
+        consumer_key: 'oAuth consumer key',
+        consumer_secret: 'oAuth consumer secret',
+        token: 'OAuth Access Token',
+        token_secret: 'OAuth Access Token Secret'
+    },
+    '500px': {
+        consumer_key: 'oAuth consumer key',
+        consumer_secret: 'oAuth consumer secret',
+    }
+};
+
+module.exports = oAuth;

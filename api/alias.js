3d1207c4ed628ae236706565d218d40eb2f09a1a
Dariusz Niemczyk <palid@o2.pl>
2014-04-17 10:12:20 +0200

    Alias command fixes

--- api/alias.js
+++ api/alias.js
@@ -2,7 +2,6 @@
 var _ = require('lodash'),
     fs = require('fs'),
     path = require('path'),
-    client = require('../config/bot.js'),
     watch = require('../initialize/watch.js'),
     hotLoad = require('../helpers/hotload.js'),
     config = require('../initialize/parseJSON.js'),
@@ -10,7 +9,6 @@ var _ = require('lodash'),
 
 watch.on('configChanged', function () {
     aliases = hotLoad(__dirname, '../initialize/createAliasDict.js');
-    config = hotLoad(__dirname, '../initialize/parseJSON.js');
 });
 
 var method = function alias(options) {
@@ -18,15 +16,26 @@ var method = function alias(options) {
         data = options.message;
 
     if (_.has(config, data)) {
-        var list = "";
-        _.forEach(config[data].aliases, function (alias, iterator) {
-            list = list + (client.commandCharacter + alias + " ");
+        var list = "",
+            objList = "";
+        _.forEach(config[data].aliases, function (property) {
+            if (_.isObject(property)) {
+                console.log(property);
+                objList = objList + (property.alias + " ");
+            } else {
+                console.log(property);
+                list = list + (property + " ");
+            }
+
         });
 
         return {
             type: "say",
             to: channel,
-            message: "Aliases for " + data + " are: " + list
+            message: [
+                "Simple aliases for " + data + ": " + list,
+                "Complex aliases for " + data + ": " + objList
+            ].join("\n\r")
         };
     } else {
         return {

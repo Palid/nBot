3d63d028e253790d54d6b2ad33650f2375b8caf4
Dariusz Niemczyk <palid@o2.pl>
2014-05-15 08:01:42 +0200

    loadDirectory changes. Database is starting to look good.

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ var loadDir = require('../helpers/loadDirectory.js');
 
 module.exports = loadDir(__dirname, {
     currentDir: __dirname,
-    type: 'js',
+    type: '.js',
     maxDepth: 0,
     event: 'apiLoaded'
 });
---
+++ core/database/schema/User.js
@@ -0,0 +1,44 @@
+var mongoose = require('../init.js'),
+    config = require('../../../config/bot.js'),
+    Schema = mongoose.Schema;
+
+var user = new Schema({
+    nick: String,
+    preferences: {
+        language: {
+            type: String,
+            default: "en"
+        },
+    },
+    permissions: {
+        group: String,
+        level: Number,
+        api: {
+            command: String,
+            level: Number
+        }
+    },
+    aliases: [{
+        alias: String,
+        addedBy: {
+            type: String,
+            default: config.options.root
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    seen: [{
+        channel: String,
+        date: Date
+    }],
+    memo: [{
+        from: String,
+        message: String,
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }]
+});

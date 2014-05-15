8daf6520cc795c04404503c9624b7e52d6124def
Dariusz Niemczyk <palid@o2.pl>
2014-05-15 11:48:02 +0200

    Error message on failure for eval.

--- cluster/eval.js
+++ cluster/eval.js
@@ -1,29 +1,36 @@
 "use strict";
-var _ = require('lodash');
+var _ = require('lodash'),
+    vm = require('vm');
+
+var context = {
+    math: Math
+};
 
 //The runner.js is ran in a separate process and just listens for the message which contains code to be executed
 process.on('message', function clusterData(data) {
-    var vm = require('vm');
+    var ctx, script, response;
 
-    var context = {
-        math: Math
-    },
-        ctx = vm.createContext(context),
-        script = vm.createScript(data, null, true),
-        response;
+    ctx = vm.createContext(context);
 
-    response = script.runInNewContext(ctx);
 
-    if (_.isString(response)) {
-        process.send(response.replace(/[\r\n]/g, '').trim());
-    } else if (_.isFunction(response)) {
-        process.send(response.toString());
-    } else if (_.isNaN(response)) {
-        process.send("NaN");
-    } else if (!process) {
-        process.send(false);
+    try {
+        script = vm.createScript(data, null, true);
+        response = script.runInNewContext(ctx);
+
+        if (_.isString(response)) {
+            process.send(response.replace(/[\r\n]/g, '').trim());
+        } else if (_.isFunction(response)) {
+            process.send(response.toString());
+        } else if (_.isNaN(response)) {
+            process.send("NaN");
+        } else if (!process) {
+            process.send(false);
+        }
+
+        process.send(response);
+    } catch (err) {
+        console.log(err);
+        process.send("Error: " + err.message);
     }
-
-    process.send(response); //Send the finished message to the parent process
 
 });

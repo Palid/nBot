d2d81eb2a7821ffca78f788d7448bea55c1ca974
Dariusz Niemczyk <palid@o2.pl>
2014-05-15 06:37:37 +0200

    Added all options to bot creation

--- core/bot.js
+++ core/bot.js
@@ -1,21 +1,27 @@
 "use strict";
-var fs = require('fs'),
-    path = require('path'),
-    irc = require('irc'),
+var irc = require('irc'),
     config = require('../config/bot.js');
 
 var client = new irc.Client(config.irc.network, config.irc.nick, {
     userName: config.irc.userName,
     realName: config.irc.realName,
     channels: config.irc.channels,
+    port: config.irc.port,
+    autoRejoin: config.irc.autoRejoin,
+    autoConnect: config.irc.autoConnect,
+    retryCount: config.irc.retryCount,
+    retryDelay: config.irc.retryDelay,
     floodProtection: config.irc.floodProtection,
     floodProtectionDelay: config.irc.floodProtectionDelay,
     messageSplit: config.irc.messageSplit,
-    sasl: config.irc.sasl,
     login: config.irc.login,
-    password: config.irc.password
+    password: config.irc.password,
+    secure: config.irc.secure,
+    selfSigned: config.irc.selfSigned,
+    certExpired: config.irc.certExpired,
+    stripColors: config.irc.stripColors,
+    channelPrefixes: config.irc.channelPrefixes
 });
-
 client.options = config.options;
 
 module.exports = client;

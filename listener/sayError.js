914512bb3440a1b82babef1b2c6c394cde632c06
Dariusz Niemczyk <palid@o2.pl>
2014-05-17 08:59:39 +0200

    Lots of changes.

--- listener/sayError.js
+++ listener/sayError.js
@@ -1,11 +1,11 @@
 "use strict";
 var client = require('../config/bot.js');
 
-function method(message) {
+function sayError(message) {
     var length = message.args.length;
     if (length === 4) {
         return client.say(message.args[2], message.args[1] + ' ' + message.args[length - 1]);
     }
 }
 
-module.exports = method;
+module.exports = sayError;

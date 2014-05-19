f5c298f8e6f61f2abbc922e79ec9c804129d23b1
Dariusz Niemczyk <palid@o2.pl>
2014-05-19 18:09:54 +0200

    Final URL regexp changes.

--- listener/commandsRegexp.js
+++ listener/commandsRegexp.js
@@ -17,8 +17,7 @@ var _ = require('lodash'),
             method: require('./methods/context/checkCommand.js')
         },
         urlTitle: {
-            re: new RegExp(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/),
-            // re: new RegExp(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})*\/?/),
+            re: new RegExp(/[-a-zA-Z0-9:_\+.~#?&//=]{2,256}\.[^@\ ][a-z]{2,12}\b(\/[-a-zA-Z0-9:%_\+.~#?&//=]*)?/ig),
             method: require('./methods/context/urlScrape.js')
         },
     },

3f4383b6a7cc3f83dade7e145e3c44fc0e0c18c6
Dariusz Niemczyk <palid@o2.pl>
2014-05-20 19:36:29 +0200

    Path fixes

--- core/initialize/database/backup.js
+++ core/initialize/database/backup.js
@@ -1,7 +1,7 @@
 "use strict";
 var fs = require('fs'),
     path = require('path'),
-    dir = path.resolve(__dirname, '../../database/db.json'),
+    dir = path.resolve(__dirname, '../../../database/db.json'),
     db = require('./db.js');
 
 // Save the base instantly
@@ -15,4 +15,4 @@ setInterval(function () {
     fs.writeFile(dir, JSON.stringify(db, null, 4), function (err) {
         if (err) console.log(err);
     });
-}, 60000);
+}, 600);

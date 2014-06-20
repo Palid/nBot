2482b44887b03ae97ca092f88c979c8f0cfb240b
Dariusz Niemczyk <palid@o2.pl>
2014-06-20 10:42:07 +0200

    Fixes to bot ignore.

--- listener/index.js
+++ listener/index.js
@@ -27,7 +27,7 @@ client.addListener('message', function (from, to, message) {
         .select('permissions.isBanned')
         .exec()
         .then(function (doc) {
-            if (!doc.permissions.isBanned) {
+            if (!doc || doc && doc.permissions && !doc.permissions.isBanned) {
                 if (nick === toLower) {
                     logger({
                         timeStamp: true,

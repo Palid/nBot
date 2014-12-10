258ced6664db6b277ac41339594b782e19ca0548
Dariusz Niemczyk <palid@o2.pl>
2014-12-10 17:43:15 +0100

    Moar catapiz!

--- api/public/catapi.js
+++ api/public/catapi.js
@@ -1,46 +1,60 @@
 "use strict";
+var _ = require('lodash');
 var request = require('request');
+var xml2js = require('xml2js').parseString;
 var rek = require('rekuire');
 var events = rek('/bot.js').events,
-    url = "http://thecatapi.com/api/images/get",
-    requestOptions = {
-        method: 'HEAD'
-    };
+  url = "http://thecatapi.com/api/images/get?format=xml&results_per_page=";
+
+
+function getKittenz(channel, formattedURL) {
+  var r = request(formattedURL, function (err, response) {
+    if (err) {
+      console.log(err);
+      r.abort();
+      events.emit('apiSay', channel, "Something's wrong with CatAPI");
+    }
+    xml2js(response.body, function(err, result){
+      if (err) console.log(err);
+      _.forEach(result.response.data[0].images[0].image, function(imageObject) {
+        events.emit('apiSay', channel, imageObject.url);
+      });
+      r.abort();
+    });
+  });
+}
 
 var method = function catsApi(options) {
-    var channel = options.to;
+  var channel = options.to,
+    msg = parseInt(options.message, 10),
+    data = (!_.isNaN(msg) && msg <= 5) ? msg : 1;
 
-    var r = request(url, requestOptions, function (err, response) {
-        if (err) {
-            console.log(err);
-            r.abort();
-            events.emit('apiSay', channel, "Something's wrong with CatAPI");
-        }
-        events.emit('apiSay', channel, response.request.uri.href);
-        r.abort();
-    });
+    var formattedURL = url + data;
 
-    return {
-        type: "async"
-    };
+    getKittenz(channel, formattedURL);
 };
 
 var defaults = {
-    description: {
-        pl: ",catapi - Zwraca losowe zdjęcia kotka.",
-        en: ",catapi - Returns a random kitten photo."
-    },
-    aliases: [
-        "cat",
-        "cats",
-        "kotki",
-        "koty",
-        "kot"
-    ]
+  description: {
+    pl: ",catapi - Zwraca losowe zdjęcia kotka.",
+    en: ",catapi - Returns a random kitten photo."
+  },
+  aliases: [
+    "cat",
+    "cats",
+    "kotki",
+    "koty",
+    "kot"
+  ]
 };
 
 
+method({
+  channel: '#nbot',
+  message: 10
+});
+
 module.exports = {
-    method: method,
-    defaults: defaults
+  method: method,
+  defaults: defaults
 };

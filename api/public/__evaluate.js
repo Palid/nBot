1ca5e8795bb775bc758e64a33262038319e4653f
Dariusz Niemczyk <palid@phoelid.me>
2018-10-30 21:30:12 +0100

    Bump eval

--- api/public/__evaluate.js
+++ api/public/__evaluate.js
@@ -1,11 +1,16 @@
 'use strict';
 var vm = require('vm');
 var _ = require('lodash');
+var moment = require('moment');
 
-var context = {};
+var context = {
+  _: _,
+  lodash: _,
+  moment: moment,
+};
 //The runner.js is ran in a separate process and just listens for the message which contains code to be executed
 process.on('message', function clusterData(data) {
-  var ctx, response;
+  var ctx, response, parsed;
 
   ctx = vm.createContext(context);
 
@@ -16,7 +21,14 @@ process.on('message', function clusterData(data) {
       var map = _.map(response, function(item) {
         return String(item).replace(/[\r\n]/g, '').trim();
       });
-      process.send(map);
+      process.send(JSON.stringify(response));
+    } else if (_.isObject(response)){
+      try {
+        process.send(JSON.stringify(response).replace(/[\r\n]/g, '').replace(/"/g, '').trim());
+      } catch (e) {
+        console.log(e.stack);
+        process.send('Error: ' + e.message);
+      }
     } else {
       process.send(String(response).replace(/[\r\n]/g, '').trim());
     }
